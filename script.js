8a7a8fe6fd7640a7b54a91032a917d65317c9b95
Renugadevi <41415275+renugaponnuchamy@users.noreply.github.com>
2023-02-20 21:53:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basics": {
+      "name": "RENUGADEVI P",
+      "email": "renuga13894@gamil.com",
+      "phone": 9080489853,
+      "degree": "B.Tech-Information Technology",
+      "location": {
+        "address": "174/B,Vatakkepattu,",
+        "postalCode": 673305,
+        "city": "quilandy",
+        "district":"kozhikode"
+        // "state": "Kerela",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          
+          "github":"https://github.com/renugaponnuchamy"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Virtuo tech",
+        "position": "Front end developer",
+        "startDate": "may-2018",
+        "endDate": "may-2019",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Mohamed Sathak Engineering college",
+        "department": "Information Technology",
+        "studyType": "fulltime",
+        "batch start year": 2011,
+        "batch end year": 2015,
+        "gpa": 6.6,
+      }
+    ],
+    "Software Proficiency": [
+      {
+        "name": "Html, Css, Bootstrap,Javascript",
+        "level": "beginner",
+   
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "reading books,Singing,Gardening",
+      }
+    ]
+  }
+  console.log(myResume);
+  
+
